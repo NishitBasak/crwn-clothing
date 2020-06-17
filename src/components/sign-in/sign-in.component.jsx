ea23340f7fd76809079c685bfc6ad9a0ed2ea6f1
Nishit Basak <n.basak16@gmail.com>
2020-06-17 17:05:29 +0530

    integrated firebase into application

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
-import FormInput from '../form-input/form-input.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 import CustomButton from '../custom-button/custom-button.component';
-
+import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
+
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -53,7 +53,14 @@ class SignIn extends React.Component {
                         label='password'
                     />
 
-                    <CustomButton type='submit'>Sign In</CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            {''}
+                        Sign in with Google {''}
+                        </CustomButton>
+                    </div>
+
                 </form>
             </div>
         );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,37 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+var config = {
+    apiKey: "AIzaSyB55XfrW7FM7xL__-DBPiIhZI4B6yxMdVo",
+    authDomain: "crwn-db-28f68.firebaseapp.com",
+    databaseURL: "https://crwn-db-28f68.firebaseio.com",
+    projectId: "crwn-db-28f68",
+    storageBucket: "crwn-db-28f68.appspot.com",
+    messagingSenderId: "575815590121",
+    appId: "1:575815590121:web:0ff83522eed7c4e0005078",
+    measurementId: "G-XL77V0MQHM"
+};
+
+/*const config = {
+    apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+    authDomain: 'crwn-db.firebaseapp.com',
+    databaseURL: 'https://crwn-db.firebaseio.com',
+    projectId: 'crwn-db',
+    storageBucket: 'crwn-db.appspot.com',
+    messagingSenderId: '850995411664',
+    appId: '1:850995411664:web:7ddc01d597846f65'
+};
+*/
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
