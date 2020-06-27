408ab52c3cd42735797f4576b552acc24fe92df0
Nishit Basak <n.basak16@gmail.com>
2020-06-27 16:50:34 +0530

    implemented firebase firestore, included ability to store authenticated users into firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,30 @@ var config = {
     measurementId: "G-XL77V0MQHM"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    // console.log(snapShot);
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('Error creating user. ', error.message);
+
+        }
+    }
+    return userRef;
+}
+
 /*const config = {
     apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
     authDomain: 'crwn-db.firebaseapp.com',
